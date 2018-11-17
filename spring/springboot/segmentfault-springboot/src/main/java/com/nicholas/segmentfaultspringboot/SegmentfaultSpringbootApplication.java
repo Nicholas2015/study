b06050a2df75482ab12e157fde0ebd8c34835889
Nicholas2015 <799327210@qq.com>
2018-11-17 10:45:52 +0800

    study

---
+++ spring/springboot/segmentfault-springboot/src/main/java/com/nicholas/segmentfaultspringboot/SegmentfaultSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.nicholas.segmentfaultspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegmentfaultSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegmentfaultSpringbootApplication.class, args);
+    }
+}
